08a2cf5c9a5b563a2e40159b9e613c738cbaa0b0
Faisal Alsofyani <fis445@hotmail.com>
2024-06-25 23:11:17 +0300

    v8

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import {useDropzone} from 'react-dropzone'
 
 
 const UserProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = useState([])
+
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/vi/user-profile").then(res => {
       console.log(res)
+      setUserProfiles(res.data)
     });
   };
 
@@ -15,8 +20,34 @@ const UserProfiles = () => {
     fetchUserProfiles();
   }, []);
 
-  return <h1>Hello</h1>
+  return userProfiles.map((userProfile, index) => {
+    return(
+      <div key={index}>
+        <MyDropzone/>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>
+    )
+  })
 };
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return <div className="App"> 
